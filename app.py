88e251072f7cba2ae5641830e2756bb8d516e69a
MarianaG10 <167145288+MarianaG10@users.noreply.github.com>
2024-08-15 10:32:45 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,3 +10,27 @@ st.image(image, caption = "Soeñoo")
 
 texto = st.text_input("Escribe algo", "Este es mi texto")
 st.write("El texto escrito es ", texto)
+
+st.subheader("Ahora usemos 2 columnas")
+
+col1, col2 = st.columns(2)
+with col1:
+  st.subheader("Esta es la primera columna")
+  st.write("Las insterfaces multimodales mejoran la experiencia de usuario")
+  resp = st.checkbox("Estoy de acuerdo")
+  if resp:
+    st.write("correcto")
+
+with col2:
+  st.subheader("Esta es la segunda columna")
+  modo == st.radio("Pelicula más vieja de Tim Burton", ("Corpse bride", "The nightmare before christmas", "Frankenweenie"))
+  if modo == "Corpse bride"
+    st.write("Es la segunda más vieja")
+  if modo == "The nightmare before christmas"
+    st.write("Es de las más nuevas")
+  if modo == "Frankenweenie"
+    st.write("sI es la más vieja de las tres")
+
+
+             
+  
